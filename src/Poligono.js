e9124242788272fd49214cc75cccc17ac2384a89
cherrux <j.tapiac2@gmail.com>
2020-08-20 18:16:04 -0400

    Update Poligono.js

--- src/Poligono.js
+++ src/Poligono.js
@@ -15,9 +15,35 @@ let Poligono = {
     return this.getValues();
   }
 
-
- 
 }
+
+//clase heredador
+function Poligono2(base, lado, altura, apotema, radio) {
+//herederos
+  Circulo.call(this, radio)
+  Decagono.call(this, lado, apotema)
+  Octagono.call(this, lado, apotema)
+  Rectangulo.call(this, base, altura)
+  Rectangulo.call(this, base, altura.lado)
+  // this.decagono2 = {
+  //   nombrePila,
+  //   apellido
+  // };
+  this.base = base;
+  this.lado = lado;
+  this.altura = altura;
+  this.apotema = apotema;
+  this.radio = radio;
+};
+
+function Profesor(nombrePila, apellido, edad, genero, intereses, materia) {
+  Person.call(this, nombrePila, apellido, edad, genero, intereses);
+
+  this.materia = materia;
+}
+
+
+
 
 //export poligono
 export default Poligono
